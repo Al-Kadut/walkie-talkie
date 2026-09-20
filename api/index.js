e0db051cad8b6aea1aa64fb28b8827fc23dff734
Al-Kadut <ajipamungkas0002@gmail.com>
2026-09-20 15:18:52 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    path: '/api/socket.io',
+    path: '/api',
     cors: {
         origin: '*',
         methods: ['GET', 'POST']
