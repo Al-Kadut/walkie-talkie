7f2fae8a650498c3c0d6246cf261ff0e242b282a
El-Kadut <ajipamungkas0002@gmail.com>
2026-09-20 14:36:06 +0700

    Add Vercel API entry

---
+++ api/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+const setupSignaling = require('../server/signaling');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+setupSignaling(io);
+
+app.get(['/', '/join/:id'], (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+module.exports = server;
